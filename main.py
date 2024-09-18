59e04ba4d96042dc375cfee67782552db75cdabc
EternalVision <eternalvision.ai@gmail.com>
2024-09-18 19:36:15 -0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import cv2
+
+# Create a VideoCapture object
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Error opening camera")
+
+# infinite loop until user decides to stop the program execution
+while True:
+    
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    # Display the resulting frame
+    cv2.imshow('Video Capture', frame)
+
+    # if 'q' is pressed on the keyboard, 
+    # stop capturing frames and close all windows
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# When everything done, release the VideoCapture object
+cap.release()
+cv2.destroyAllWindows()
