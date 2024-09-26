f8dbb4a2f76148612743a53e2f9846431a9fc66f
EternalVision <eternalvision.ai@gmail.com>
2024-09-26 14:02:04 -0300

    1

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 
 # Access the Arducam CSI camera
 # cap = cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(3840), height=(2160), format=(NV12), framerate=(30/1) ! nvvidconv ! video/x-raw, format=(BGRx) ! videoconvert ! appsink", cv2.CAP_GSTREAMER)
-cap = cv2.VideoCapture("/dev/video0")
+cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
     print("Cannot open camera")
