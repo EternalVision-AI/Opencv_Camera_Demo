5ded4a2038b0ef3b85818f739e9781e5e1aec287
EternalVision <eternalvision.ai@gmail.com>
2024-09-26 13:19:49 -0300

    Added count

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ cap = cv2.VideoCapture(1)
 if not cap.isOpened():
     print("Error opening camera")
 
+count = 0
 # infinite loop until user decides to stop the program execution
 while True:
     
@@ -15,9 +16,10 @@ while True:
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-
+    count += 1
+    print(count)
     # Display the resulting frame
-    cv2.imshow('Video Capture', frame)
+    # cv2.imshow('Video Capture', frame)
 
     # if 'q' is pressed on the keyboard, 
     # stop capturing frames and close all windows
@@ -25,5 +27,5 @@ while True:
         break
 
 # When everything done, release the VideoCapture object
-cap.release()
-cv2.destroyAllWindows()
+# cap.release()
+# cv2.destroyAllWindows()
