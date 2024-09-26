97332289688bd6a1e701bb868b5c902c4e55d883
EternalVision <eternalvision.ai@gmail.com>
2024-09-26 13:54:16 -0300

    1

--- main.py
+++ main.py
@@ -1,31 +1,23 @@
 import cv2
 
-# Create a VideoCapture object
-cap = cv2.VideoCapture(0)
+# Access the Arducam CSI camera
+cap = cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(3840), height=(2160), format=(NV12), framerate=(30/1) ! nvvidconv ! video/x-raw, format=(BGRx) ! videoconvert ! appsink", cv2.CAP_GSTREAMER)
 
 if not cap.isOpened():
-    print("Error opening camera")
+    print("Cannot open camera")
+    exit()
 
-count = 0
-# infinite loop until user decides to stop the program execution
 while True:
-    
-    # Capture frame-by-frame
     ret, frame = cap.read()
-    
+
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-    count += 1
-    print(count)
-    # Display the resulting frame
-    # cv2.imshow('Video Capture', frame)
+    print("Running...")
+    # cv2.imshow('Arducam Camera', frame)
 
-    # if 'q' is pressed on the keyboard, 
-    # stop capturing frames and close all windows
     if cv2.waitKey(1) == ord('q'):
         break
 
-# When everything done, release the VideoCapture object
-# cap.release()
-# cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
